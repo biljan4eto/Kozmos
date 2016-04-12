1ea501a24c52f19dd17f12077b525b2e928704e0
Matjaž Mav <Matja Mav>
2016-04-12 20:44:14 +0200

    done we give up.... :,(

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ kozmos.config(['$routeProvider', function($routeProvider) {
 kozmos.factory('appSettings', [function() {
     return {
         // "baseUrl": "http://localhost"
-        "baseUrl": "http://matjazmav.github.com/Kozmos"
+        "baseUrl": "http://matjazmav.github.io/Kozmos"
     };
 }]);
 
